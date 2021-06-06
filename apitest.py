aec71cf3be16a96a1517c21dd3072549938d1981
DerSchokolo <frfranzen@gmail.com>
2021-06-06 21:01:50 +0200

    first

---
+++ apitest.py
@@ -0,0 +1,95 @@
+import json
+import requests
+
+#Title
+print("")
+print("#########################################")
+print("#########################################")
+print("#########################################")
+print("Corona Tracker v1.0")
+print("Oberhavel, Brandenburg, Deutschland")
+print("#########################################")
+print("")
+
+
+
+
+
+#Oberhavel
+url_oberhavel = "https://api.corona-zahlen.org/districts"
+resp_oberhavel = requests.get(url=url_oberhavel)
+data_oberhavel = resp_oberhavel.json()
+
+incidence_oberhavel = data_oberhavel["data"]["12065"]["weekIncidence"]
+
+print("Incidence Oberhavel:")
+print(round(incidence_oberhavel, 1))
+
+
+#Break
+print("")
+print("#########################################")
+print("")
+
+
+#Brandenburg
+url_brandenburg = "https://api.corona-zahlen.org/states/BB"
+resp_brandenburg = requests.get(url=url_brandenburg)
+data_brandenburg = resp_brandenburg.json()
+
+incidence_brandenburg = data_brandenburg["data"]["BB"]["weekIncidence"]
+
+print("Incidence Brandenburg:")
+print(round(incidence_brandenburg, 1))
+
+
+
+#Break
+print("")
+print("#########################################")
+print("")
+
+
+
+#Deutschland
+url_deutschland = "https://api.corona-zahlen.org/germany"
+resp_deutschland = requests.get(url=url_deutschland)
+data_deutschland = resp_deutschland.json()
+
+incidence_deutschland = data_deutschland["weekIncidence"]
+
+print("Incidence Deutschland:")
+print(round(incidence_deutschland, 1))
+
+
+#Break
+print("")
+
+
+
+#Deutschland
+url_impfungen = "https://api.corona-zahlen.org/vaccinations"
+resp_impungen = requests.get(url=url_impfungen)
+data_impfungen = resp_impungen.json()
+
+incidence_impfungen = data_impfungen["data"]["vaccinated"]
+
+print("Anzahl aller 1. Geimpften in Deutschland:")
+incidence_impfungen = incidence_impfungen / 83020000
+incidence_impfungen = incidence_impfungen * 100
+incidence_impfungen = round(incidence_impfungen, 1)
+incidence_impfungen = str(incidence_impfungen)
+
+print(incidence_impfungen + "%")
+
+
+
+#End
+print("")
+print("#########################################")
+print("#########################################")
+print("#########################################")
+
+
+#Formating
+#print(json.dumps(data_impfungen, indent=4))
